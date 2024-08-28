8e7f1ac78019aa1ec79103e708d801dbf8854a9e
jesskabasilio <jessica.limabasilio@gmail.com>
2024-08-27 22:20:37 -0300

    finalizando projeto

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,8 @@
 const toggleTheme = document.getElementById('toggleTheme');
 const rootHtml = document.documentElement
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
+
 
 function changeTheme(){
     const currenTheme = rootHtml.getAttribute("data-theme");
@@ -12,4 +15,39 @@ function changeTheme(){
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+      const accordionItem = header.parentElement;
+      const accordionActive = accordionItem.classList.contains("active");
+  
+      accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+  })
+  
+  menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+      menuLinks.forEach(i => i.classList.remove("active"));
+      item.classList.add("active");
+    })
+  })
+
+// Função para adicionar redirecionamento a botões
+function addRedirectEvent(buttonClass) {
+  const buttons = document.querySelectorAll(buttonClass);
+  
+  buttons.forEach(button => {
+      button.addEventListener('click', function() {
+          const url = button.getAttribute('data-url');
+          window.location.href = url;
+      });
+  });
+}
+
+// Adiciona evento para os botões de Prévia
+addRedirectEvent('.redirect-btn');
+
+// Adiciona evento para os botões de Repositório
+addRedirectEvent('.repo-btn');
+
 
